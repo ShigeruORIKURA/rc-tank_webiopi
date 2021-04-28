70deb1eb501d51cb5c3b9236d8ebc98e237bbd47
Shigeru ORIKURA <shigeruorikura@gmail.com>
2021-04-28 19:55:44 +0900

    fix servo control

--- items/servo.py
+++ items/servo.py
@@ -11,30 +11,31 @@ L_SERVO_CH = 11
 
 MIN_SERVO_PULSE_WIDH = 150
 MAX_SERVO_PULSE_WIDH = 450
-r_servo_val=MIN_SERVO_PULSE_WIDH
-l_servo_val=MIN_SERVO_PULSE_WIDH
+r_servo_val = MIN_SERVO_PULSE_WIDH
+l_servo_val = MIN_SERVO_PULSE_WIDH
 
-if len(sys.argv) < 2:
-    return 0
+def main():
 
-servo_state = int(sys.argv[2])
+    if len(sys.argv) < 2:
+        return 0
 
-while True:
-    global servo_state
-    sleep(0.1)
+    servo_state = int(sys.argv[2])
 
-    if 'right' == sys.argv[0]:
-        global r_servo_val
-        r_servo_val = r_servo_val + servo_state
-        if r_servo_val < MIN_SERVO_PULSE_WIDH or r_servo_val > MAX_SERVO_PULSE_WIDH:
-            r_servo_val = r_servo_val - servo_state
-        else:
-            pwm.set_pwm(R_SERVO_CH, 0, r_servo_val)
+    while True:
+        sleep(0.1)
 
-    if 'left' == sys.argv[0]:
-        global l_servo_val
-        l_servo_val = l_servo_val + servo_state
-        if l_servo_val < MIN_SERVO_PULSE_WIDH or l_servo_val > MAX_SERVO_PULSE_WIDH:
-            l_servo_val = l_servo_val - servo_state
-        else:
-            pwm.set_pwm(L_SERVO_CH, 0, l_servo_val)
+        if 'right' == sys.argv[0]:
+            global r_servo_val
+            r_servo_val = r_servo_val + servo_state
+            if r_servo_val < MIN_SERVO_PULSE_WIDH or r_servo_val > MAX_SERVO_PULSE_WIDH:
+                r_servo_val = r_servo_val - servo_state
+            else:
+                pwm.set_pwm(R_SERVO_CH, 0, r_servo_val)
+
+        if 'left' == sys.argv[0]:
+            global l_servo_val
+            l_servo_val = l_servo_val + servo_state
+            if l_servo_val < MIN_SERVO_PULSE_WIDH or l_servo_val > MAX_SERVO_PULSE_WIDH:
+                l_servo_val = l_servo_val - servo_state
+            else:
+                pwm.set_pwm(L_SERVO_CH, 0, l_servo_val)
